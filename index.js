694b3158f7a96aa1b98e3b1aac114c707cc43eec
Tom Poidevin <tom.poidevin@gmail.com>
2023-01-20 12:46:21 +0100

    cors

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const db = require('@cyclic.sh/dynamodb')
+const cors = require('cors')
+
+app.use(cors({
+  origin: '*'
+}));
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
