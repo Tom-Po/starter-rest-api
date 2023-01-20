e441d824ee7077ba0163ebed7dd276b13eb9e57f
Tom Poidevin <tom.poidevin@gmail.com>
2023-01-20 13:03:30 +0100

    fix request

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post('/todos', async (req, res) => {
   console.log(req.body)
   const col = req.params.data
   console.log(`from collection: ${col} add with params ${JSON.stringify(req.body)}`)
-  const item = await db.collection(col).set(req.body)
+  const item = await db.collection("todos").set(req.body)
   console.log(JSON.stringify(item, null, 2))
   res.json(item).end()
 })
